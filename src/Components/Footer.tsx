f4265984b82776e0eb4c59678b56d6b66f02aae7
root <root@v2202402196447257645.megasrv.de>
2025-02-16 17:33:44 +0100

    save

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -11,25 +11,25 @@ const Footer = () => {
 
                     <Grid.Col xs={12} sm={8} md={8} lg={8}>
                         
-                        <Text size="xl" weight={700} color="white" mb="10px">Danaxssert International Trading GmbH</Text> 
+                        <Text size="xl" weight={700} color="white" mb="10px">DANAXSSERT International Trading GmbH</Text> 
                         <Text color="white" mb="5px">
-                            Tel:  0049 1607683035
+                            Tel:  +49 1607683035
                         </Text>
                         <Text color="white" mb="5px"> 
-                            Addr: Kronprinzenstr.14 65185 Germany
+                            Addr: Kronprinzenstr.14, 65185 Wiesbaden, Germany
                         </Text>
 
                         <Text color="white" mb="20px">
-                            Email: sales@danaxxsert.de <Anchor href="mailto:https://sales@danaxssert.de"></Anchor>
+                            Email: sales@danaxssert.de <Anchor href="mailto:sales@danaxssert.de"></Anchor>
                         </Text>
 
-                        <Button variant="white" color="black" onClick={() => redirectToLink("mailto:https://sales@danaxssert.de")} > Send email to us</Button>
+                        <Button variant="white" color="black" onClick={() => redirectToLink("mailto:sales@danaxssert.de")} > Send email to us</Button>
                     </Grid.Col>
 
                     <Grid.Col xs={12} sm={4} md={4} lg={4}>
                         <Code color="yellow" style={{ display: 'flex', flexDirection: 'column', gap: 6, padding: 15 }}>
                             T#######
-                            <Anchor href="mailto:https://sales@danaxssert.de">
+                            <Anchor href="mailto:sales@danaxssert.de">
                                 <UnstyledButton>
                                     <Group>
                                         <Avatar size={40} color="orange">DANAXSSERT</Avatar>
